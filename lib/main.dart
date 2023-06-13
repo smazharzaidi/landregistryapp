651cd1ab05cf708fdbadd4b076dd2e6865230af6
smazharzaidi <35042724+smazharzaidi@users.noreply.github.com>
2023-06-14 00:12:15 +0500

    marking functionality

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'sale_purchase.dart';
 import 'signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(apiKey: 'AIzaSyCJW60r7xh8sUlEiS-hGgaeiFybH2QWa0I', appId: '1:699692984203:ios:7b347eb500994f7115b18e', messagingSenderId: '699692984203', projectId: 'landregistry-15941'),
+    options: FirebaseOptions(
+        apiKey: 'AIzaSyCJW60r7xh8sUlEiS-hGgaeiFybH2QWa0I',
+        appId: '1:699692984203:ios:7b347eb500994f7115b18e',
+        messagingSenderId: '699692984203',
+        projectId: 'landregistry-15941'),
   );
   runApp(const MainApp());
 }
@@ -20,7 +25,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.orange),
       debugShowCheckedModeBanner: false,
-      home: SignUp(),
+      home: HomePage(),
     );
   }
 }
--- lib/sale_purchase.dart
+++ lib/sale_purchase.dart
@@ -15,166 +15,190 @@ class SalePurchase extends StatefulWidget {
 class _SalePurchaseState extends State<SalePurchase> {
   final TextEditingController cnicController = TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
   GoogleMapController? _mapController;
+  Set<Circle> _circles = {};
+  Set<Polyline> _polylines = {};
+  List<LatLng> _polygonPoints = [];
+  Marker? _selectedMarker;
+
+  LatLng? _midPoint;
+  Circle? _midPointCircle;
+  Set<Marker> _markers = {};
+
+  @override
+  void dispose() {
+    cnicController.dispose();
+    _mapController?.dispose();
+    super.dispose();
+  }
+
+  void _openDrawer(BuildContext context) {
+    _scaffoldKey.currentState?.openDrawer();
+  }
+
+  void _submitForm() {
+    // Handle form submission
+  }
+
+  void _addMarker(LatLng position) {
+    final markerId = MarkerId(position.toString());        //ye masla hy
+
+    Marker? marker;
+
+    for (var m in _markers) {
+      if (m.position.latitude == position.latitude &&
+          m.position.longitude == position.longitude) {
+        marker = m;
+        break;
+      }
+    }
+
+    if (marker != null) {
+      setState(() {
+        _selectedMarker = marker;
+      });
+    } else {
+      Marker newMarker = Marker(
+        markerId: markerId,
+        position: position,
+        draggable: true,
+        onDragEnd: (newPosition) {
+          _onMarkerDragEnd(markerId, newPosition);
+        },
+        infoWindow: InfoWindow(
+          title: 'Marker',
+        ),
+      );
+
+      setState(() {
+        _markers.add(newMarker);
+        _polygonPoints.add(position);
+        _updatePolygons();
+      });
+    }
+  }
+
+  void _onMarkerDragEnd(MarkerId markerId, LatLng newPosition) {
+    if (_selectedMarker != null && _selectedMarker!.markerId == markerId) {
+      final updatedMarkers = _markers.map((marker) {
+        if (marker.markerId == markerId) {
+          return marker.copyWith(positionParam: newPosition);
+        }
+        return marker;
+      }).toSet();
+
+      final updatedPolygonPoints = _polygonPoints.map((point) {
+        if (point == _selectedMarker!.position) {
+          return newPosition;
+        }
+        return point;
+      }).toList();
+
+      setState(() {
+        _markers = updatedMarkers;
+        _polygonPoints = updatedPolygonPoints;
+        _updatePolygons();
+      });
+    }
+  }
+
+  void _updatePolygons() {
+    setState(() {
+      _polylines.clear();
+
+      if (_polygonPoints.length > 1) {
+        final List<LatLng> polygonPointsCopy = List.from(_polygonPoints);
+        polygonPointsCopy.add(_polygonPoints[0]);
+
+        _polylines.add(
+          Polyline(
+            polylineId: PolylineId('boundary'),
+            points: polygonPointsCopy,
+            color: Colors.blue,
+            width: 2,
+          ),
+        );
+      }
+    });
+  }
+
+  // void _updateMidPoint() {
+  //   if (_polygonPoints.length > 1) {
+  //     final double sumLat =
+  //         _polygonPoints.fold(0, (prev, element) => prev + element.latitude);
+  //     final double sumLng =
+  //         _polygonPoints.fold(0, (prev, element) => prev + element.longitude);
+  //     final int count = _polygonPoints.length;
+
+  //     final LatLng midPoint = LatLng(sumLat / count, sumLng / count);
+
+  //     if (_midPoint == null) {
+  //       _midPoint = midPoint;
+  //       _addMidPointCircle();
+  //     } else {
+  //       setState(() {
+  //         _midPoint = midPoint;
+  //         _moveMidPointCircle();
+  //       });
+  //     }
+  //   } else {
+  //     setState(() {
+  //       _midPoint = null;
+  //       _midPointCircle = null;
+  //     });
+  //   }
+  // }
+
+  // void _addMidPointCircle() {
+  //   final Circle midPointCircle = Circle(
+  //     circleId: CircleId('midPoint'),
+  //     center: _midPoint!,
+  //     radius: 10, // Adjust the radius to your preference
+  //     fillColor: Colors.red.withOpacity(0.5),
+  //     strokeWidth: 0,
+  //     consumeTapEvents: true,
+  //   );
+
+  //   setState(() {
+  //     _midPointCircle = midPointCircle;
+  //   });
+  // }
+
+  // void _moveMidPointCircle() {
+  //   final Circle updatedMidPointCircle =
+  //       _midPointCircle!.copyWith(centerParam: _midPoint!);
+
+  //   final updatedCircles = Set<Circle>.from(_circles);
+  //   updatedCircles.remove(_midPointCircle);
+  //   updatedCircles.add(updatedMidPointCircle);
+
+  //   setState(() {
+  //     _midPointCircle = updatedMidPointCircle;
+  //     _circles = updatedCircles;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            backgroundColor: Colors.transparent,
-            leading: Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(
-                    Icons.menu,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    _scaffoldKey.currentState?.openDrawer();
-                  },
-                );
-              },
-            ),
-            stretch: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(
-                'Sell Your Land',
-                style: GoogleFonts.sora(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              stretchModes: const <StretchMode>[
-                StretchMode.zoomBackground,
-                StretchMode.blurBackground,
-                StretchMode.fadeTitle
-              ],
-              centerTitle: true,
-              collapseMode: CollapseMode.parallax,
-              background: Stack(
-                fit: StackFit.expand,
-                children: [
-                  Image.asset(
-                    'assets/theme.jpg',
-                    fit: BoxFit.cover,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withOpacity(0.7),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            floating: true,
-            pinned: true,
-            snap: true,
-            expandedHeight: 200,
+      appBar: AppBar(
+        title: Text(
+          'Sell Your Land',
+          style: GoogleFonts.sora(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
-          SliverFillRemaining(
-            child: GestureDetector(
-              onScaleStart: (details) {
-                // Handle scale start
-              },
-              onScaleUpdate: (details) {
-                // Handle scale update
-              },
-              onScaleEnd: (details) {
-                // Handle scale end
-              },
-              child: GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: CameraPosition(
-                  target: LatLng(37.4419, -122.1419),
-                  zoom: 15,
-                ),
-                onMapCreated: (controller) {
-                  _mapController = controller;
-                },
-                gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
-                  Factory<OneSequenceGestureRecognizer>(
-                      () => ScaleGestureRecognizer()),
-                ].toSet(),
-              ),
-            ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
           ),
-          SliverToBoxAdapter(
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Enter buyer\'s CNIC:',
-                        style: GoogleFonts.sora(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 8,
-                          ),
-                          child: TextField(
-                            controller: cnicController,
-                            decoration: const InputDecoration(
-                              hintText: 'CNIC',
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(
-                      20,
-                    ), // Adjust the radius to your preference
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Button onPressed logic
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                            20,
-                          ), // Adjust the radius to match the ClipRRect
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Submit',
-                            style: GoogleFonts.sora(),
-                          ),
-                          const Icon(Icons.arrow_forward),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          onPressed: () {
+            _openDrawer(context);
+          },
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -196,12 +220,73 @@ class _SalePurchaseState extends State<SalePurchase> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const HelpSupportPage()),
+                      builder: (context) => HelpSupportPage()),
                 );
               },
             ),
           ],
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(37.4419, -122.1419),
+                zoom: 15,
+              ),
+              markers: _markers,
+              circles: _circles,
+              polylines: _polylines,
+              onTap: _addMarker,
+              onMapCreated: (controller) {
+                _mapController = controller;
+              },
+              gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
+                Factory<OneSequenceGestureRecognizer>(
+                    () => ScaleGestureRecognizer()),
+              ].toSet(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Enter buyer\'s CNIC:',
+                  style: GoogleFonts.sora(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  controller: cnicController,
+                  decoration: const InputDecoration(
+                    hintText: 'CNIC',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Submit',
+                        style: GoogleFonts.sora(),
+                      ),
+                      const Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
