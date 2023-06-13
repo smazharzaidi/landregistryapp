590e1e35d26e334df406da03929d10cf74bfd31d
inbisat-git <inbisat.zahra28@gmail.com>
2023-06-13 23:14:25 +0500

    Kuch bhi 2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,8 +5,75 @@ import 'sale_purchase.dart';
 import 'login.dart';
 import 'profile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  List<HelpTopic> helpTopics = [
+    HelpTopic(
+      question: 'How do I reset my password?',
+      answer: 'To reset your password, go to the login screen and tap on the Forgot Password link. Follow the instructions provided to reset your password.',
+    ),
+    HelpTopic(
+      question: 'How can I contact customer support?',
+      answer: 'For any assistance or queries, you can reach our customer support team by emailing support@example.com or by calling +1-123-456-7890.',
+    ),
+    HelpTopic(
+      question: 'What payment methods are accepted?',
+      answer: 'We accept all major credit cards, including Visa, Mastercard, and American Express. You can also pay using PayPal.',
+    ),
+    HelpTopic(
+      question: 'How do I set up my system?',
+      answer: 'To reset your password, go to the login screen and tap on the Forgot Password link. Follow the instructions provided to reset your password.',
+    ),
+  ];
+
+  late List<AnimationController> _animationControllers;
+  late List<Animation<double>> _animations;
+  late List<bool> expansionStates;
+
+  @override
+  void initState() {
+    super.initState();
+    expansionStates = List<bool>.generate(helpTopics.length, (index) => false);
+    _animationControllers = List<AnimationController>.generate(
+      helpTopics.length,
+      (index) => AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 300),
+      ),
+    );
+    _animations = List<Animation<double>>.generate(
+      helpTopics.length,
+      (index) => CurvedAnimation(
+        parent: _animationControllers[index],
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _animationControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _toggleExpansion(int index) {
+    setState(() {
+      expansionStates[index] = !expansionStates[index];
+      if (expansionStates[index]) {
+        _animationControllers[index].forward();
+      } else {
+        _animationControllers[index].reverse();
+      }
+    });
+  }
 
   void _openDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
@@ -55,7 +122,9 @@ class HomePage extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                       builder: (context) => SalePurchase(key: UniqueKey())),
-                );
+               
+
+ );
               },
             ),
             ListTile(
@@ -74,31 +143,54 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 200,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset(
-                'assets/theme.jpg',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Center(
-              child: Text(
-                'Welcome to the Home Page!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      body: ListView.builder(
+        itemCount: helpTopics.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ExpansionPanelList(
+              elevation: 1,
+              expandedHeaderPadding: EdgeInsets.zero,
+              expansionCallback: (panelIndex, isExpanded) {
+                _toggleExpansion(index);
+              },
+              children: [
+                ExpansionPanel(
+                  headerBuilder: (context, isExpanded) {
+                    return ListTile(
+                      title: Text(
+                        helpTopics[index].question,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    );
+                  },
+                  body: AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                    height: expansionStates[index] ? null : 0.0,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Opacity(
+                        opacity: expansionStates[index] ? 1.0 : 0.0,
+                        child: Text(helpTopics[index].answer),
+                      ),
+                    ),
+                  ),
+                  isExpanded: expansionStates[index],
                 ),
-              ),
+              ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
+}
+
+class HelpTopic {
+  final String question;
+  final String answer;
+
+  HelpTopic({required this.question, required this.answer});
 }
